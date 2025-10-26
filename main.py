1dea95824a3ba4b1c28302e22df6942971f1098e
shinwooseong <sillyday01@gmail.com>
2025-10-26 21:40:32 +0900

    StateMachine 클래스 추가 - 상태변환 위한 클래스

--- main.py
+++ main.py
@@ -2,9 +2,34 @@
 
 from pico2d import *
 import time
+from sdl2 import SDL_KEYDOWN, SDL_KEYUP, SDLK_RIGHT, SDLK_LEFT, SDLK_UP, SDLK_DOWN
+
 
 SCREEN_W, SCREEN_H = 800, 600
 SPRITE_W, SPRITE_H = 70, 82
+
+class StateMachine:
+    def __init(self,start_state,transitions):
+        self.next_state = None
+        self.cur_state = start_state
+        self.transitions = transitions
+        self.cur_state.enter(None)
+
+    def update(self):
+        self.cur_state.do()
+
+    def draw(self):
+        self.cur_state.draw()
+
+    def handle_state_event(self, state_event):
+        if state_event[0] in self.transitions[self.cur_state]:
+            next_state_func = self.transitions[self.cur_state][state_event[0]]
+            next_state = next_state_func(state_event)
+            self.cur_state.exit(state_event)
+            self.cur_state = next_state
+            self.cur_state.enter(state_event)
+            return True
+        return False
 
 class Idle:
     def __init__(self):
