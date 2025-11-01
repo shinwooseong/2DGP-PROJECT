47b7cffdaf5ee8fcac0baf22ff82b26b7ebce57b
shinwooseong <sillyday01@gmail.com>
2025-11-01 22:40:33 +0900

    !!-몬스터 클래스 설정 __ 대충 만들기 성공

--- Monster.py
+++ Monster.py
@@ -1,32 +1,57 @@
 from pico2d import load_image
+import time
+import math
 
 class Monster:
-    def __init__(self,name='monster',image_path='assets/monsters/monster.png', x=100, y=100, hp=10, speed=0, attack=0):
+    def __init__(self,name='monster',image_path='assets/monsters/monster.png', x=100, y=100, hp=10, speed=0, attack=0, attack_range=0):
         self.name = name
-        self.image_path = image_path
+        self.image = image_path
         self.x = x
         self.y = y
         self.hp = hp
         self.speed = speed
         self.attack = attack
+        self.attack_range = attack_range
         self.alive = True
+        self.dir = 1
+
+        # 공격 시간
+        self.attack_cooldown = 0.5
+        self.last_attack_time = 0.0
+
 
     def draw(self):
         self.image.draw(self.x, self.y)
 
-    def update(self):
+    def update(self,dt):
         # 몬스터의 상태 업데이트 로직 추가
-        pass
+        if not self.alive:
+            return
+        self.x += self.speed * self.dir * dt
 
-    def attack(self)
+    # target = 주인공
+    def attack(self, target = None):
         # 몬스터 공격 로직 추가:
-        pass
+        now = time.time()
+        if now - self.last_attack_time < self.attack_cooldown:
+            return False
+        self.last_attack_time = now
 
-    def move(self):
-        # 몬스터 이동 로직 추가
-        pass
+        if target is None:
+            return True
+
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp <= 0:
+            self.alive = False
+
+    def move(self,dx, dy):
+        self.x += dx
+        self.y += dy
+
 
     def range_of_attack(self, target):
-        # 공격 범위 내에 있는지 확인하는 로직 추가
-        pass
+        dx = target.x - self.x
+        dy = target.y - self.y
+        return dx * dx + dy * dy <= self.attack_range * self.attack_range
 
