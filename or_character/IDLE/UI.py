20e955ae4d7e92bb409f9b123594124fc9f0ce10
shinwooseong <sillyday01@gmail.com>
2025-11-04 14:28:50 +0900

    UI 만들기 위한 UI.py 제작

---
+++ or_character/IDLE/UI.py
@@ -0,0 +1,21 @@
+from pico2d import load_image
+
+
+
+SCREEN_W, SCREEN_H = 1000, 1000
+
+class UI:
+    def __init__(self):
+        pass
+
+    def load_UI_image(self):
+        pass
+
+
+    def handle_event(self, event):
+        # 이벤트 처리
+        pass
+
+    def draw(self):
+        # 배낭 UI 그리기
+        pass
