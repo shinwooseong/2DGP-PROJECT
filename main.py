301a0a8577a8306a6548b1777a97580ca88d5111
shinwooseong <sillyday01@gmail.com>
2025-11-29 21:30:03 +0900

    main 에서 게임 시작 전 플레이어를 딱 한 번만 생성하여 server에 저장하게 수정

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from pico2d import *
 import game_framework
-#import play_mode
-import shop_mode as start_mode
+import village_mode as start_mode
+import main_chracter
+import server
 
 # 스크린 크기는 main_chracter 모듈에서 가져옴
 import main_chracter
 
 open_canvas(main_chracter.SCREEN_W, main_chracter.SCREEN_H,sync=True)
+# 게임 시작 전 플레이어를 딱 한 번만 생성하여 server에 저장
+server.player = main_chracter.Main_character()
+
 # game_framework를 실행하고 시작 모드를 지정
 game_framework.run(start_mode)
 close_canvas()
